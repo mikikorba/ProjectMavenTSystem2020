8c1cd8ddb1f21ebafba59978a632cc85ec643bca
BenkoMatej <benkomatej45445@mail.com>
2020-01-11 15:43:45 +0100

    updated application.properties|jobsEntity|testControler

--- src/main/java/tjobs/controller/TestController.java
+++ src/main/java/tjobs/controller/TestController.java
@@ -1,5 +1,13 @@
 package tjobs.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +15,10 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import tjobs.entity.jobsEntity;
 
@@ -19,12 +31,77 @@ public class TestController {
 
 	@RequestMapping("/")
 	public String index() {
-		
-//		jobsService.addComment(new jobsEntity("hello"));
-		
-		System.out.println(jobsService.getAllJobs().get(0).getDgkjhb());
-		
+//		jobsService.addComment(new jobsEntity("yap,yap,yap"));
+//		System.out.println(jobsService.getAllJobs().get(0).getDgkjhb());
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				boolean executeAdding = true;
+				List<jobsEntity> inDatabase = jobsService.getAllJobs();
+				List<jobsEntity> addToDatabase = new ArrayList<>();
+				try {
+					JsonArray tmp = pffff();
+					for (int i = 0; i < tmp.size(); i++) {
+						addToDatabase.add(new jobsEntity(tmp.get(i).getAsJsonObject()));
+					}
+					if (addToDatabase.isEmpty()) {
+						executeAdding = false;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					executeAdding = false;
+				}
+				if (executeAdding) {
+					for (jobsEntity je : addToDatabase) {
+						boolean add = true;
+						for (jobsEntity dat : inDatabase) {
+							if (je.getID().equals(dat.getID())) {
+								add = false;
+							}
+						}
+						if (add) {
+							jobsService.addComment(je);
+							System.out.println("ADDED: |ID:"+je.getID() + " |IDENT:"+je.getIdent());
+						}
+
+					}
+				}
+
+			}
+		}).start();
+
 		return "index";
 	}
 
+	private JsonArray pffff() throws MalformedURLException, Exception {
+		String quarry = "{\"JobadID\":\"\",\"LanguageCode\":\"2\",\"SearchParameters\":{\"FirstItem\":1,\"CountItem\":100,\"Sort\":[{\"Criterion\":\"FavoriteJobIndicator\",\"Direction\":\"DESC\"}],\"MatchedObjectDescriptor\":[\"ID\",\"RelevanceRank\",\"PositionID\",\"PositionTitle\",\"ParentOrganization\",\"ParentOrganizationName\",\"PositionURI\",\"PositionLocation.CountryName\",\"PositionLocation.CountrySubDivisionName\",\"PositionLocation.CityName\",\"PositionLocation.Longitude\",\"PositionLocation.Latitude\",\"PositionIndustry.Name\",\"JobCategory.Name\",\"CareerLevel.Name\",\"PositionSchedule.Name\",\"PublicationStartDate\",\"UserArea.TextWorkingHoursPerWeek\",\"PublicationEndDate\",\"ApplicationDeadline\",\"UserArea.TextRequiredWorkExperience\",\"UserArea.TextTravel\",\"UserArea.TextRequiredLanguageSkills\",\"UserArea.TextJobDescription\",\"UserArea.TextRequirementDescription\",\"PositionBenefit.Code\",\"PositionBenefit.Name\",\"FavoriteJobIndicator\",\"FavoriteJobIndicatorName\",\"PositionStartDate\",\"Partnerhochschule\"]},\"SearchCriteria\":[{\"CriterionName\":\"PositionLocation.Latitude\",\"CriterionValue\":[\"48.7163857\"]},{\"CriterionName\":\"PositionLocation.Longitude\",\"CriterionValue\":[\"21.2610746\"]},{\"CriterionName\":\"PositionLocation.Distance\",\"CriterionValue\":[\"13.975119302957982\"]},{\"CriterionName\":\"PositionLocation.CountryCode\",\"CriterionValue\":[\"SK\"]},{\"CriterionName\":\"PositionLocation.AreaCode\",\"CriterionValue\":[\"SK\"]},{\"CriterionName\":\"ParentOrganization\",\"CriterionValue\":[\"5460\",\"1278\"]},{\"CriterionName\":\"PositionLocation.City\",\"CriterionValue\":[\"Ko�ice, Slovakia\"]},{\"CriterionName\":\"PositionLocation.Country\",\"CriterionValue\":[\"29\"]}]}";
+		String json = getJson(new URL("https://t-systems.jobs/globaljobboard_api/v3/search/"), quarry);
+		Gson gs = new Gson();
+		JsonObject jsonObject = gs.fromJson(json, JsonObject.class);
+		JsonArray lol = jsonObject.get("SearchResult").getAsJsonObject().get("SearchResultItems").getAsJsonArray();
+		return lol;
+	}
+
+	private String getJson(URL url, String quarry) throws Exception {
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Type", "application/json; utf-8");
+		connection.setRequestProperty("Accept", "application/json");
+		connection.setDoOutput(true);
+		OutputStream out = connection.getOutputStream();
+		byte[] input = quarry.getBytes("utf-8");
+		out.write(input, 0, input.length);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+		StringBuilder response = new StringBuilder();
+		String responseLine = null;
+		while ((responseLine = reader.readLine()) != null) {
+			response.append(responseLine.trim());
+		}
+		String output = response.toString();
+		out.close();
+		reader.close();
+
+		return output;
+	}
 }
--- src/main/java/tjobs/entity/jobsEntity.java
+++ src/main/java/tjobs/entity/jobsEntity.java
@@ -6,22 +6,234 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import com.google.gson.JsonObject;
+
 @Entity
 public class jobsEntity {
 	@Id
 	@GeneratedValue
 	private int ident;
 
-	String dgkjhb;
+	private String MatchedObjectId = "";
+	private String ApplicationDeadline = "";
+	private String PositionTitle = "";
+	private String PositionLocation_CityName = "";
+	private String PositionLocation_CountryName = "";
+//	private String TextRequirementDescription = ""; // html
+//	private String TextJobDescription = ""; // html
+	private String PublicationEndDate = "";
+	private String PositionSchedule = "";
+	private String PositionURI = "";
+	private String ID = "";
+	private String JobCategory = "";
+	private String PublicationStartDate = "";
+	private String PositionBenefit_Code = "";
+	private String PositionBenefit_Name = "";
+	private String CareerLevel = "";
+	private String PositionID = "";
+	private String ParentOrganizationName = "";
 
 	public jobsEntity() {
 	}
 
-	public jobsEntity(String dgkjhb) {
-		this.dgkjhb = dgkjhb;
+	public jobsEntity(JsonObject mehelo) {
+		MatchedObjectId = mehelo.get("MatchedObjectId").toString();
+		JsonObject tmp = mehelo.get("MatchedObjectDescriptor").getAsJsonObject();
+//		ApplicationDeadline = tmp.get("ApplicationDeadline").toString();
+//		System.out.println(tmp.get("ApplicationDeadline").toString());
+//		ApplicationDeadline = tmp.get("ApplicationDeadline").toString();// there is some nullPointerExeptions TODO make it work
+		PositionTitle = tmp.get("PositionTitle").toString();
+		if (tmp.get("PositionLocation").getAsJsonArray().size() > 0) {
+			JsonObject tmpLocation = tmp.get("PositionLocation").getAsJsonArray().get(0).getAsJsonObject();
+			PositionLocation_CityName = tmpLocation.get("CityName").toString();
+			PositionLocation_CountryName = tmpLocation.get("CountryName").toString();
+		}
+		JsonObject tmpUserArea = tmp.get("UserArea").getAsJsonObject();
+//		TextRequirementDescription = tmpUserArea.get("TextRequirementDescription").toString();
+//		TextJobDescription = tmpUserArea.get("TextJobDescription").toString();
+		PublicationEndDate = tmp.get("PublicationEndDate").toString();
+		if (tmp.get("PositionSchedule").getAsJsonArray().size() > 0) {
+			JsonObject tmpPositionSchedule = tmp.get("PositionSchedule").getAsJsonArray().get(0).getAsJsonObject();
+			PositionSchedule = tmpPositionSchedule.get("Name").toString();
+		}
+		PositionURI = tmp.get("PositionURI").toString();
+		ID = tmp.get("ID").toString();
+		if (tmp.get("JobCategory").getAsJsonArray().size() > 0) {
+			JsonObject tmpJobCategory = tmp.get("JobCategory").getAsJsonArray().get(0).getAsJsonObject();
+			JobCategory = tmpJobCategory.get("Name").toString();
+		}
+		PublicationStartDate = tmp.get("PublicationStartDate").toString();
+		if (tmp.get("PositionBenefit").getAsJsonArray().size() > 0) {
+			JsonObject tmpPositionBenefit = tmp.get("PositionBenefit").getAsJsonArray().get(0).getAsJsonObject();
+			PositionBenefit_Code = tmpPositionBenefit.get("Code").toString();
+			PositionBenefit_Name = tmpPositionBenefit.get("Name").toString();
+		}
+		if (tmp.get("PositionBenefit").getAsJsonArray().size() > 0) {
+			JsonObject tmpCareerLevel = tmp.get("PositionBenefit").getAsJsonArray().get(0).getAsJsonObject();
+			CareerLevel = tmpCareerLevel.get("Name").toString();
+		}
+		PositionID = tmp.get("PositionID").toString();
+		ParentOrganizationName = tmp.get("ParentOrganizationName").toString();
 	}
 
-	public String getDgkjhb() {
-		return dgkjhb;
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("MatchedObjectId : " + MatchedObjectId + "\r\n");
+		sb.append("ApplicationDeadline : " + ApplicationDeadline + "\r\n");
+		sb.append("PositionTitle : " + PositionTitle + "\r\n");
+		sb.append("PositionLocation_CityName : " + PositionLocation_CityName + "\r\n");
+		sb.append("PositionLocation_CountryName : " + PositionLocation_CountryName + "\r\n");
+//		sb.append("TextRequirementDescription : " + TextRequirementDescription + "\r\n");
+//		sb.append("TextJobDescription : " + TextJobDescription + "\r\n");
+		sb.append("PublicationEndDate : " + PublicationEndDate + "\r\n");
+		sb.append("PositionSchedule : " + PositionSchedule + "\r\n");
+		sb.append("PositionURI : " + PositionURI + "\r\n");
+		sb.append("ID : " + ID + "\r\n");
+		sb.append("JobCategory : " + JobCategory + "\r\n");
+		sb.append("PublicationStartDate : " + PublicationStartDate + "\r\n");
+		sb.append("PositionBenefit_Code : " + PositionBenefit_Code + "\r\n");
+		sb.append("PositionBenefit_Name : " + PositionBenefit_Name + "\r\n");
+		sb.append("CareerLevel : " + CareerLevel + "\r\n");
+		sb.append("PositionID : " + PositionID + "\r\n");
+		sb.append("ParentOrganizationName : " + ParentOrganizationName + "\r\n");
+		return sb.toString();
 	}
+
+	public int getIdent() {
+		return ident;
+	}
+
+	public void setIdent(int ident) {
+		this.ident = ident;
+	}
+
+	public String getMatchedObjectId() {
+		return MatchedObjectId;
+	}
+
+	public void setMatchedObjectId(String matchedObjectId) {
+		MatchedObjectId = matchedObjectId;
+	}
+
+	public String getApplicationDeadline() {
+		return ApplicationDeadline;
+	}
+
+	public void setApplicationDeadline(String applicationDeadline) {
+		ApplicationDeadline = applicationDeadline;
+	}
+
+	public String getPositionTitle() {
+		return PositionTitle;
+	}
+
+	public void setPositionTitle(String positionTitle) {
+		PositionTitle = positionTitle;
+	}
+
+	public String getPositionLocation_CityName() {
+		return PositionLocation_CityName;
+	}
+
+	public void setPositionLocation_CityName(String positionLocation_CityName) {
+		PositionLocation_CityName = positionLocation_CityName;
+	}
+
+	public String getPositionLocation_CountryName() {
+		return PositionLocation_CountryName;
+	}
+
+	public void setPositionLocation_CountryName(String positionLocation_CountryName) {
+		PositionLocation_CountryName = positionLocation_CountryName;
+	}
+
+	public String getPublicationEndDate() {
+		return PublicationEndDate;
+	}
+
+	public void setPublicationEndDate(String publicationEndDate) {
+		PublicationEndDate = publicationEndDate;
+	}
+
+	public String getPositionSchedule() {
+		return PositionSchedule;
+	}
+
+	public void setPositionSchedule(String positionSchedule) {
+		PositionSchedule = positionSchedule;
+	}
+
+	public String getPositionURI() {
+		return PositionURI;
+	}
+
+	public void setPositionURI(String positionURI) {
+		PositionURI = positionURI;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getJobCategory() {
+		return JobCategory;
+	}
+
+	public void setJobCategory(String jobCategory) {
+		JobCategory = jobCategory;
+	}
+
+	public String getPublicationStartDate() {
+		return PublicationStartDate;
+	}
+
+	public void setPublicationStartDate(String publicationStartDate) {
+		PublicationStartDate = publicationStartDate;
+	}
+
+	public String getPositionBenefit_Code() {
+		return PositionBenefit_Code;
+	}
+
+	public void setPositionBenefit_Code(String positionBenefit_Code) {
+		PositionBenefit_Code = positionBenefit_Code;
+	}
+
+	public String getPositionBenefit_Name() {
+		return PositionBenefit_Name;
+	}
+
+	public void setPositionBenefit_Name(String positionBenefit_Name) {
+		PositionBenefit_Name = positionBenefit_Name;
+	}
+
+	public String getCareerLevel() {
+		return CareerLevel;
+	}
+
+	public void setCareerLevel(String careerLevel) {
+		CareerLevel = careerLevel;
+	}
+
+	public String getPositionID() {
+		return PositionID;
+	}
+
+	public void setPositionID(String positionID) {
+		PositionID = positionID;
+	}
+
+	public String getParentOrganizationName() {
+		return ParentOrganizationName;
+	}
+
+	public void setParentOrganizationName(String parentOrganizationName) {
+		ParentOrganizationName = parentOrganizationName;
+	}
+	
 }
