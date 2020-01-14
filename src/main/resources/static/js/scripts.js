7d49bd11202bba0de9df0ddb07341fb6a15a48c5
Lafayett <Lafayett@147.232.169.113>
2020-01-14 16:20:16 +0100

    Talbe filling and formating with Javascript

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,39 +1,3 @@
-//setInterval(function() {
-//    var elem = document.getElementById("progress");
-//    var id = setInterval(bar, 800);
-//    function bar() {
-//            elem.style.animation = "loader 15s linear infinite";
-//            }
-//}, 3000);
-
-//function showOrHideTable() {
-//    table = document.getElementById("myTable");
-//    rows = table.rows;
-//
-//    if (table == null || table == undefined) {
-//        for (i = 2; i < (rows.length - 1); i++) {
-//        x = rows[i].getElementsByTagName("td")[0];
-//        x.setAttribute("id", "hide");   
-//    } else {
-//            for (i = 2; i < (rows.length - 1); i++) {
-//            x = rows[i].getElementsByTagName("td")[0];
-//            x.setAttribute("id", "show");       
-//        }
-//    }
-//}
-//    
-//$('#myTable th').each(function(i) {
-//    var remove = 0;
-//
-//    var tds = $(this).parents('table').find('tr td:nth-child(' + (i + 1) + ')')
-//    tds.each(function(j) { if (this.innerHTML == '') remove++; });
-//
-//    if (remove == ($('#myTable tr').length - 1)) {
-//        $(this).hide();
-//        tds.hide();
-//    }
-//});
-
 setInterval(function sortTable() {
     
     var table, rows, style, i, x, y;
@@ -51,44 +15,58 @@ setInterval(function sortTable() {
     document.getElementById("job").setAttribute("id", "hide");
 }, 15000);
 
-function loadDoc(coll, content, row) {
+
+function loadDoc(id, col, row) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementsByClassName(content).innerHTML = this.responseText;
+            document.getElementsByClassName(id)[0].innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "/profile?collum=" + coll + "&row=" + row, true);
+    xhttp.open("GET", "/profile?collum="+col+"&row="+row, true);
     xhttp.send();
 }
 
 function onload(){
 	  var table = document.getElementById("myTable");
 	  for (i = 1; i < 30; i++) {
-	  	var row = table.insertRow( i + 1);
+	  	var row = table.insertRow(i + 1);  	
+        document.getElementsByTagName("tr")[2].setAttribute("id","active");
+        
+        
+	  	var cell0 = row.insertCell(0);
+	  	cell0.className = 'hotjob'+i;
+	  	cell0.setAttribute("style", "width:2.3vw; padding-left: 0.9vw");
 	  	
-	  	var cell1 = row.insertCell(0);
-	  	cell1.className = 'hotjob';
-	  	var cell2 = row.insertCell(1);
-	  	cell2.className = 'job';
-	 	var cell3 = row.insertCell(2);
-	  	cell3.className = 'location';
-	  	var cell4 = row.insertCell(3);
-	  	cell4.className = 'posted';
-	 	var cell5 = row.insertCell(4);
-	  	cell5.className = 'benefits';
-	  	var cell6 = row.insertCell(5);
-	  	cell6.className = 'url';
+	  	var cell1 = row.insertCell(1);
+	  	cell1.className = 'job-title'+i;
+	  	cell1.setAttribute("style", "width:50vw; font-weight: 700;text-transform: uppercase; font-size: 1.4vw; line-height: normal");
 
-	  	for (i = 1; i < 30; i++) {
- 	  		cell1.innerHTML = loadDoc(???????, hotjob, i);
-	  		cell2.innerHTML = loadDoc(position_title, job, i);
-	  		cell3.innerHTML = loadDoc(position_location_city_name, location, i);
-	  		cell4.innerHTML = loadDoc(publication_start_date, posted, i);
-	  		cell5.innerHTML = loadDoc(???????, beneftits, i);
-	  		cell6.innerHTML = loadDoc(???????, url, i);
-	  	}
+	  	var cell2 = row.insertCell(2);
+	  	cell2.className = 'location'+i;
+	  	cell2.setAttribute("style", "width:24.3vw; font-weight: 400; font-size: 1.4vw; line-height: normal");
+
+	  	var cell3 = row.insertCell(3);
+	  	cell3.className = 'posted'+i;
+	  	cell3.setAttribute("style", "width:10vw; font-weight: 400; font-size: 1.4vw; line-height: normal");
+
+	 	var cell4 = row.insertCell(4);
+	  	cell4.className = 'benefits'+i;
+	  	cell4.setAttribute("style", "width:5vw; font-weight: 400; font-size: 1.4vw; line-height: normal");
+
+	  	var cell5 = row.insertCell(5);
+	  	cell5.className = 'url'+i;
+	  	cell5.setAttribute("style", "text-aling:center");
+
+	
+//	  	loadDoc("hotjob"+i,"position_title",i);
+	  	loadDoc("job-title"+i,"position_title",i);
+	  	loadDoc("location"+i,"position_location_city_name," + "" + "position_location_country_name",i);
+	  	loadDoc("posted"+i,"publication_start_date",i);
+//	  	loadDoc("benefits"+i,"position_benefit_name",i);  
+//	  	loadDoc("url"+i,"position_title",i);  
+		
+
 	 }
 }
-
 
